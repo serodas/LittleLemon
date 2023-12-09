7645102cee237e272fced82eee4f0313f5996590
Samir Rodas <serodas9710@gmail.com>
2023-12-09 17:53:41 -0500

    Add /menu endpoint

--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('items/', views.MenuItemsView.as_view()),
+    path('<int:pk>', views.SingleMenuItemView.as_view()),
 ]
--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,6 +1,31 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from rest_framework.views import APIView
+from rest_framework import generics, status
+from rest_framework.response import Response
+from .serializers import BookingSerializer, MenuSerializer
+from .models import Booking,Menu
 
 # Create your views here.
 def home(request):
     return render(request, 'index.html', {})
+
+# Create your views here. 
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class BookingView(APIView):
+    def get(self, request):
+        items = Booking.objects.all()
+        serializer = BookingSerializer(items, many=True)
+        return Response(serializer.data)
+    def post(self, request):
+        serializer = BookingSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"status": "success", "data": serializer.data})
+        return Response(serializer.errors)
