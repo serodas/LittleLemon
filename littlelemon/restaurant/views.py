f1910af8c1c60a5df8636626b8c36fa31f8b9dac
Samir Rodas <serodas9710@gmail.com>
2023-12-09 18:20:53 -0500

    Add /booking endpoint

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from rest_framework.views import APIView
 from rest_framework import generics, status
+from rest_framework import viewsets
 from rest_framework.response import Response
 from .serializers import BookingSerializer, MenuSerializer
 from .models import Booking,Menu
@@ -18,14 +18,6 @@ class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
 
-class BookingView(APIView):
-    def get(self, request):
-        items = Booking.objects.all()
-        serializer = BookingSerializer(items, many=True)
-        return Response(serializer.data)
-    def post(self, request):
-        serializer = BookingSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response({"status": "success", "data": serializer.data})
-        return Response(serializer.errors)
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
